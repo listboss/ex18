26ab26a16654c7b4bb08d6ffdf1161c170342432
Hamid Ghadyani <hamid@hamid.cc>
2017-01-03 18:13:01 -0500

    Solution for both parts of puzzle.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,13 @@
 struct Grid([[char; 100]; 100]);
 
+impl Copy for Grid {}
+
+impl Clone for Grid {
+    fn clone(&self) -> Grid {
+        *self
+    }
+}
+
 impl Grid {
     fn new(inp: &str) -> Grid {
         fn initiate(grid: &mut Grid, inp: &str) {
@@ -22,10 +30,17 @@ impl Grid {
         }
         grid
     }
+
+    fn set(&mut self, (i, j): (usize, usize), c: char) {
+        assert!(['#', '.'].contains(&c));
+        assert!(i < self.0.len());
+        assert!(j < self.0.len());
+        self.0[i][j] = c;
+    }
 }
 
-pub trait Light {
-    fn switch(&mut self);
+trait Light {
+    fn step(&mut self, stuck_corners: bool);
     fn brightness(&self) -> usize;
 }
 
@@ -34,25 +49,65 @@ impl Light for Grid {
         self.0
             .iter()
             .map(|l| l.iter().filter(|&&c| c == '#').count())
-            .fold(0, |accum, c| accum + c)
-        // self.0
-        //     .iter()
-        //     .map(|l| l.iter().map(|&c| if c == '#' { 1 } else { 0 }).sum::<usize>())
-        //     .sum()
+            .sum()
     }
 
-    fn switch(&mut self) {
-        unimplemented!();
+    fn step(&mut self, stuck_corners: bool) {
+        let mut new_grid = self.clone();
+        let nrows = self.0.len();
+        let ncols = nrows;
+        for (i, row) in self.0.iter().enumerate() {
+            let next_i = if i != nrows - 1 { Some(i + 1) } else { None };
+            let prev_i = if i != 0 { Some(i - 1) } else { None };
+            for (j, light) in row.iter().enumerate() {
+                let next_j = if j != ncols - 1 { Some(j + 1) } else { None };
+                let prev_j = if j != 0 { Some(j - 1) } else { None };
+                let neighbors = [(prev_i, prev_j),
+                                 (prev_i, Some(j)),
+                                 (prev_i, next_j),
+                                 (Some(i), prev_j),
+                                 (Some(i), next_j),
+                                 (next_i, prev_j),
+                                 (next_i, Some(j)),
+                                 (next_i, next_j)]
+                    .iter()
+                    .filter(|&&(i, j)| i.is_some() && j.is_some())
+                    .map(|&(i, j): &(Option<usize>, Option<usize>)| (i.unwrap(), j.unwrap()))
+                    .collect::<Vec<_>>();
+                let no_neighbors_that_are_on =
+                    neighbors.iter().filter(|&&(i, j)| self.0[i][j] == '#').count();
+                match *light {
+                    '#' if ![2, 3].contains(&no_neighbors_that_are_on) => new_grid.0[i][j] = '.',
+                    '.' if no_neighbors_that_are_on == 3 => new_grid.0[i][j] = '#',
+                    _ => (),
+                }
+            }
+        }
+        if stuck_corners {
+            new_grid.0[0][0] = '#';
+            new_grid.0[0][99] = '#';
+            new_grid.0[99][0] = '#';
+            new_grid.0[99][99] = '#';
+        }
+        *self = new_grid;
     }
 }
 fn main() {
-    let grid = Grid::new(include_str!("../input.txt"));
-
-    for (_, row) in grid.0.iter().enumerate() {
-        for col in row.iter() {
-            print!("{}", col);
-        }
+    let mut grid = Grid::new(include_str!("../input.txt"));
+    for _ in 1..101 {
+        grid.step(false);
     }
+    println!("No of ON lighs: {}", grid.brightness());
+
+    let mut grid = Grid::new(include_str!("../input.txt"));
+    grid.set((0, 0), '#');
+    grid.set((0, 99), '#');
+    grid.set((99, 0), '#');
+    grid.set((99, 99), '#');
+    for _ in 1..101 {
+        grid.step(true);
+    }
+    println!("No of ON lighs with stuck corner: {}", grid.brightness());
 }
 
 #[cfg(test)]
@@ -71,5 +126,11 @@ mod tests {
                         ..##.#..####.###........#.";
         let expected = expected.chars().collect::<Vec<char>>();
         assert_eq!(expected[..], g.0[90][..]);
+    }
+
+    #[test]
+    fn getting_brightness() {
+        let g = Grid::new(include_str!("../input.txt"));
+        assert_eq!(5076, g.brightness());
     }
 }
