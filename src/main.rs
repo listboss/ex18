0118b7d50443f5534a9caacd36cc244efdae35fd
Hamid Ghadyani <hamid@hamid.cc>
2017-01-03 04:29:47 -0500

    Consolidate creation and initialization of Grid.

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,26 @@
 struct Grid([[char; 100]; 100]);
 
 impl Grid {
-    fn new() -> Grid {
-        Grid([['.'; 100]; 100])
-    }
+    fn new(inp: &str) -> Grid {
+        fn initiate(grid: &mut Grid, inp: &str) {
+            inp.lines()
+                .enumerate()
+                .map(|(x, line): (usize, &str)| {
+                    line.chars()
+                        .enumerate()
+                        .filter(|&(_, s)| s == '#')
+                        .map(|(y, s): (usize, char)| grid.0[x][y] = s)
+                        .count();
+                })
+                .count();
+        }
+        let mut grid = Grid([['.'; 100]; 100]);
 
-    fn initiate(&mut self, inp: &str) {
-        inp.lines()
-            .enumerate()
-            .map(|(x, line): (usize, &str)| {
-                line.chars()
-                    .enumerate()
-                    .filter(|&(_, s)| s == '#')
-                    .map(|(y, s): (usize, char)| self.0[x][y] = s)
-                    .count();
-            })
-            .count();
+        // If input string is empty, return a blank grid with default value.
+        if inp.len() != 0 {
+            initiate(&mut grid, inp);
+        }
+        grid
     }
 }
 
@@ -41,8 +46,7 @@ impl Light for Grid {
     }
 }
 fn main() {
-    let mut grid = Grid::new();
-    grid.initiate(include_str!("../input.txt"));
+    let grid = Grid::new(include_str!("../input.txt"));
 
     for (_, row) in grid.0.iter().enumerate() {
         for col in row.iter() {
@@ -57,8 +61,7 @@ mod tests {
 
     #[test]
     fn initiate_grid() {
-        let mut g = Grid::new();
-        g.initiate(include_str!("../input.txt"));
+        let g = Grid::new(include_str!("../input.txt"));
         let expected = "..####.####.##.#.##....#...##....#..#....#..###..#...#..###.#####.....\
                         #####..##.#.#.#.#.#.##.####...";
         let expected = expected.chars().collect::<Vec<char>>();
